46cb541d1cca2a549a5e42fc017d47d721f517a9
Furkan Ayan <ayanf16@itu.edu.tr>
2025-03-23 05:30:55 +0300

    Removed odin

--- Assets/Scripts/Managers/EnemySpawner.cs
+++ Assets/Scripts/Managers/EnemySpawner.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using ObjectsType;
-using Sirenix.OdinInspector;
 using UnityEngine;
 using Zenject;
 using Random = UnityEngine.Random;
--- Assets/Scripts/Managers/TowerPlacementManager.cs
+++ Assets/Scripts/Managers/TowerPlacementManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
-using Sirenix.OdinInspector;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.Serialization;
